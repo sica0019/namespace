7bf41285b026cc553478277e5f52c86c1d0f4836
Thomas Sicard <sica0019@algonquinlive.com>
2018-10-26 19:33:20 -0400

    Push 1

---
+++ namespace.js
@@ -0,0 +1,34 @@
+/**************************
+Filename: namespace.js
+Author: Thomas Sicard
+Date: 10/26/2018
+Description: namespace assignment
+***************************/
+
+
+var sica0019 = {
+    init: function () {
+        let awesomeDiv = document.createElement("div");
+
+        awesomeDiv.className = "box";
+
+        awesomeDiv.textContent = "Sica0019";
+
+        let boxes = document.getElementById("boxes");
+
+        boxes.appendChild(awesomeDiv);
+
+
+        awesomeDiv.addEventListener("mouseover", mouseOver);
+
+
+
+
+    }
+
+}
+
+function mouseOver() {
+    awesomeDiv.classList.toggle("highlight");
+
+}
