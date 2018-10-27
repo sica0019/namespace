47f239318543939d43087a652a463cca80d3a106
Thomas Sicard <sica0019@algonquinlive.com>
2018-10-26 20:09:22 -0400

    push 3

--- namespace.js
+++ namespace.js
@@ -5,30 +5,30 @@ Date: 10/26/2018
 Description: namespace assignment
 ***************************/
 
-
 var SICA0019 = {
-    init: function () {
+    init: function (){
         let awesomeDiv = document.createElement("div");
-
         awesomeDiv.className = "box";
-
-        awesomeDiv.textContent = "Sica0019";
-
-        let boxes = document.getElementById("boxes");
-
-        boxes.appendChild(awesomeDiv);
-
-
-        awesomeDiv.addEventListener("mouseover", mouseOver);
-
-
-
-
+        awesomeDiv.textContent = "sica0019";
+        let a = document.querySelector("#boxes");
+        a.appendChild(awesomeDiv);
+        
+        
+        awesomeDiv.addEventListener("mouseover", () => {
+            this.classList.toggle("highlight");
+      
+        });
+        awesomeDiv.addEventListener("click", () => {
+            awesomeDiv.style.backgroundColor = "red";
+            awesomeDiv.style.color = "orange";
+            awesomeDiv.style.borderBottomColor = "blue";
+            awesomeDiv.style.borderTopColor = "green";
+            awesomeDiv.style.borderLeftColor = "pink";
+            awesomeDiv.style.borderRightColor = "yellow";
+        });
+        awesomeDiv.addEventListener("mouseout", () => {
+            this.classList.toggle("highlight");
+        });
+        
     }
-
-}
-
-function mouseOver() {
-    awesomeDiv.classList.toggle("highlight");
-
 }
